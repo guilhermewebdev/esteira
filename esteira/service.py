3cd4f8859f81aaea277208f5af0bfeeb0cafc2c1
Guilherme Isaias <guilherme@guilhermeweb.dev>
2021-01-17 14:34:01 -0300

    Criação da classe service

---
+++ esteira/service.py
@@ -0,0 +1,22 @@
+from .task import Task
+
+
+class Service(Task):
+    service = None
+    host = ''
+
+    def __init__(self, image_name, host=None, external_envs={}):
+        if host:
+            self.host = host
+        else:
+            self.host = image_name.split(':')[0]
+        super().__init__(external_envs=external_envs, image=image_name)
+
+    def run(self, variables={}):
+        variables.update(self.variables)
+        self.container = self.client.containers.run(
+            self.image,
+            detach=True,
+            environment=variables,
+            hostname=str(self.host)
+        )
